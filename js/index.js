84f8d35fedd99c1b612742c5c39d3cddefd44e29
denis <danchic95@gmail.com>
2018-11-29 15:28:59 +0500

    modal window open/close with class toggle

--- js/index.js
+++ js/index.js
@@ -2,29 +2,23 @@ var btn 	   = document.querySelector(".header-main button"),
  	popup 	   = document.querySelector(".modal-window"),
  	close 	   = document.querySelector("#close"),
 	overlay    = document.querySelector(".modal-overlay"),
-	secBotBtn  = document.querySelector("#sec-page-bot-btn"),
   	bottomLink = document.querySelector("#bottom-link");
 
 btn.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	overlay.style.display = "block";
-	popup.style.display = "block";
+	popup.classList.add("open");
+	overlay.classList.add("open");
 });
 
 bottomLink.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	overlay.style.display = "block";
-	popup.style.display = "block";
-});
-
-secBotBtn.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	overlay.style.display = "block";
-	popup.style.display = "block";
+	popup.classList.add("open");
+	overlay.classList.add("open");
 });
 
 close.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	popup.style.display = "none";
-	overlay.style.display = "none";
+	popup.classList.remove("open");
+	overlay.classList.remove("open");
 });
+
